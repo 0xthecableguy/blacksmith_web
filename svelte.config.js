012b568d9bec0bac85f4478113b14374d1128445
0xthecableguy <0xthecableguy@proton.me>
2025-02-17 11:15:59 +0300

    page deployment #5

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
+
 	kit: {
 		adapter: adapter({
+			fallback: null,
 			strict: true,
-			fallback: null
 		}),
 		paths: {
 			base: '/blacksmith_web'
 		}
-	},
-	preprocess: vitePreprocess()
+	}
 };
 
 export default config;
